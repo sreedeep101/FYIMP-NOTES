a7ce8dfd010e33a778102c1f2eac2a402a937ccc
Sreedeep Pradeep <pradeepantkmalappattam@gmail.com>
2026-07-17 06:55:31 +0530

    Implement NumPy array calculations for mean, sum, max
    This script initializes a NumPy array and computes its mean, sum, and maximum values, displaying the results.

---
+++ PYTHON/Sem5_prog03.py
@@ -0,0 +1,10 @@
+#Create a NumPy array and calculate its mean, sum, and maximum.
+
+import numpy as np 
+
+array = np.array([23,55,22,53,44,34])
+
+print("array : " , array)
+print("mean  : " , np.mean(array))
+print("sum   : " , np.sum(array))
+print("maximum : " , np.max(array))
